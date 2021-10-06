f43ee019858497d5d8942efec652add4c5433fa4
Laura Murria <laura.murria@nesta.org.uk>
2021-10-06 17:08:13 +0100

    Remove header. Stats display

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,18 @@ import { calculateEquivalents } from './calculator';
 function App() {
   const usage = 100;
   const data = calculateEquivalents(usage);
+  console.log(`Data ${data}`);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       {data.equivalents.map(stat => {
+        return (
           <div key={stat.name} >
             <p>
             {stat.name}
             {stat.desc}
             {stat.value}
             </p>
-          </div>
+          </div>);
         })}
     </div>
   );
