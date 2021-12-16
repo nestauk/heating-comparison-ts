8e43ee4f6783135ba9e9ac3bb80be1fc916a3fe3
Laura Murria <laura.murria@nesta.org.uk>
2021-12-16 10:01:00 +0000

    Revert "Remove learn more button"
    This reverts commit 01a80cc95c18258d8ea44f413c552c51f5e4dfbb.

--- src/Report.tsx
+++ src/Report.tsx
@@ -95,13 +95,15 @@ export function ReportReduction(props: {
       </Grid>
     </Grid>
     <Grid container flexDirection="row" justifyContent="space-between" >
-            <p/>
             <button
             className="btn btn--secondary"
             onClick={() => reset()}
             >
             Start again
             </button>
+            <a href={redirectUrl} className="btn btn--secondary">
+            Learn more
+            </a>
     </Grid>
     </>
   );
